23c35612af6ff93a874bb48c382e496217e38d7e
Christian Stein <sormuras@gmail.com>
2024-05-20 12:06:54 +0200

    Create package-info.java

---
+++ package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2024 Christian Stein
+ * Licensed under the Universal Permissive License v 1.0 -> https://opensource.org/license/upl
+ */
+
+/** Provides information about JUnit's modules and tools. */
+package run.duke.info.junit;
